8e914481bb6ec83cd6b5082b53cc21fd14487f3c
davidghidini03-maker <davidghidini03@gmail.com>
2026-09-05 14:42:26 -0300

    Dia 02 - calculadora

---
+++ dia02.py
@@ -0,0 +1,12 @@
+# Calculadora do dia 02
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+soma = n1 + n2
+sub = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+print(f"\nResultados:")
+print(f"Soma: {soma}")
+print(f"Subtração: {sub}")
+print(f"Multiplicação: {mult}")
+print(f"Divisão: {div}")
