3e86772eeca58708f197e047f9472eb15845125e
davidghidini03-maker <davidghidini03@gmail.com>
2026-09-17 00:28:14 -0300

    Em frente!

--- dia01.py
+++ dia01.py
@@ -4,9 +4,15 @@ print("Olá, Digimundo!") # Imprime uma mensagem de saudação
 print("Este é o meu primeiro programa em Python.") # Imprime uma mensagem informativa
 
 nome = "David" # Atribui o nome "David" à variável 'nome'
-idade = 39 # Atribui a idade 25 à variável 'idade'
+idade = 39 # Atribui a idade 39 à variável 'idade'
 print("Meu nome é", nome, "e eu tenho", idade, "anos.") # Imprime uma mensagem com o nome e a idade
 
 # Booleanas
 é_adulto = idade >= 21 # Verifica se a idade é maior ou igual a 21
 print("Eu sou adulto:", é_adulto) # Imprime uma mensagem indicando se a pessoa é adulta ou não
+tem_filhos = True
+estrangeiro = False
+
+# Interpolação de Strings (f-strings)
+print(f'Tem Filhos: {tem_filhos}') # Imprime uma mensagem indicando se a pessoa tem filhos ou não
+print(f'Estrangeiro: {estrangeiro}') # Imprime uma mensagem indicando se a pessoa é estrangeira ou não
