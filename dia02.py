ec64245c116c9779bcf3d440d4cce3f80a21a8ed
davidghidini03-maker <davidghidini03@gmail.com>
2026-09-17 00:33:15 -0300

    Talvez iniciar um projeto?

--- dia02.py
+++ dia02.py
@@ -1,17 +1,12 @@
-# Dia 02 Desafio Extra
-nome = input("Qual é o seu nome? ")
-n1 = float(input("Digite o primeiro número: "))
-n2 = float(input("Digite o segundo número: "))
-soma = n1 + n2
-sub = n1 - n2
-mult = n1 * n2
-div = n1 / n2
-resto = n1 % n2
-potencia = n1 ** n2
-print(f"\n--- Resultados para {nome} ---")
-print(f"Soma: {soma}")
-print(f"Subtração: {sub}")
-print(f"Multiplicação: {mult}")
-print(f"Divisão: {div:.2f}")
-print(f"Resto da Divisão: {resto}")
-print(f"{n1} elevado a {n2} = {potencia}")
+# Calculadora
+n1 = float(input("Digite o primeiro número: ")) # Solicita ao usuário que digite o primeiro número e converte a entrada para um número de ponto flutuante
+n2 = float(input("Digite o segundo número: ")) # Solicita ao usuário que digite o segundo número e converte a entrada para um número de ponto flutuante
+soma = n1 + n2 # Calcula a soma dos dois números
+sub = n1 - n2 # Calcula a subtração do primeiro número pelo segundo
+mult = n1 * n2 # Calcula a multiplicação dos dois números
+div = n1 / n2 # Calcula a divisão do primeiro número pelo segundo
+print(f"\nResultados:") # Imprime uma mensagem indicando que os resultados serão exibidos
+print(f"Soma: {soma}") # Imprime o resultado da soma dos dois números
+print(f"Subtração: {sub}") # Imprime o resultado da subtração do primeiro número pelo segundo
+print(f"Multiplicação: {mult}") # Imprime o resultado da multiplicação dos dois números
+print(f"Divisão: {div}") # Imprime o resultado da divisão do primeiro número pelo segundo
