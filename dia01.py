84adc78164b9c434ed117cbdb752095804a53bfd
davidghidini03-maker <davidghidini03@gmail.com>
2026-09-17 00:06:26 -0300

    Mensagem de commit vazia.

--- dia01.py
+++ dia01.py
@@ -1,4 +1,8 @@
-nome = input("Fala seu nome: ")
-print(f"Salve, {nome}! Funcionou")
-for i in range(1, 6):
-    print(f"Contando: {i}")
+# Começando
+print("Olá, Digimundo!") # Imprime uma mensagem de saudação
+
+print("Este é o meu primeiro programa em Python.") # Imprime uma mensagem informativa
+
+nome = "David" # Atribui o nome "David" à variável 'nome'
+idade = 39 # Atribui a idade 25 à variável 'idade'
+print("Meu nome é", nome, "e eu tenho", idade, "anos.") # Imprime uma mensagem com o nome e a idade
