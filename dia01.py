3b3f4e9fa1ec063712e43228ae3ea999974fb583
davidghidini03-maker <davidghidini03@gmail.com>
2026-09-17 00:17:33 -0300

    Continue tentando!

--- dia01.py
+++ dia01.py
@@ -6,3 +6,7 @@ print("Este é o meu primeiro programa em Python.") # Imprime uma mensagem infor
 nome = "David" # Atribui o nome "David" à variável 'nome'
 idade = 39 # Atribui a idade 25 à variável 'idade'
 print("Meu nome é", nome, "e eu tenho", idade, "anos.") # Imprime uma mensagem com o nome e a idade
+
+# Booleanas
+é_adulto = idade >= 21 # Verifica se a idade é maior ou igual a 21
+print("Eu sou adulto:", é_adulto) # Imprime uma mensagem indicando se a pessoa é adulta ou não
